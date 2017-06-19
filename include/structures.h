e1cf1a2ece1a0529b2132f1478c82d42b8f48f42
churmd <churmdaniel@gmail.com>
2017-06-19 15:11:22 +0100

    Added individual class

--- include/structures.h
+++ include/structures.h
@@ -1,6 +1,26 @@
 #ifndef STRUCTURE_H
 #define STRUCTURE_H
 
+class Individual {
+  private :
+    int len;
+    int *rowValues;
 
+  public :
+    Individual(int length) {
+      len = length;
+      rowValues = new int[length];
+    }
+    ~Individual() {
+      delete rowValues;
+    }
+    int getLen(){
+      return len;
+    }
+    int getRowValue(int col);
+    int setRowValue(int col, int val);
+    void randomValues();
+    string toString();
+}
 
 #endif
