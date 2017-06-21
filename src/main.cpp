664d93439eb45e56c496156f3b6d4a0400faba91
churmd <churmdaniel@gmail.com>
2017-06-21 14:54:20 +0100

    Changed main to take in arguements so that users can define the problem constraints (number of queens, number of loop iterations, unlimited iterations)

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 #include "structures.h"
 #include "edaAlgo.h"
-
+/*
 int main()
 {
     cout << "Hello world!" << endl;
@@ -27,9 +27,43 @@ int main()
     int a = pd->getValue(1,1);
     cout << a << endl;
 
-    Individual *best = edaLoop(6, 50000);
+    Individual *best = edaLoop(5, 50000, 1);
     cout << best->toString() << endl;
     cout << fitness(best) << endl;
 
     return 0;
+}
+*/
+
+int main(int argc, char const *argv[]) {
+  int queenNum = 1;
+  int maxIter = 1000;
+  int iterModifier = 1;
+  try {
+    if(argc < 2){
+        cout << "No arguements passed, defualt values used." << endl;
+        cout << "Usage: ./nqueens <number of queens> <maximum number of iterations> <iteration increment>" << endl;
+        cout << "Set iteration increment to 0 for an unlimited number of iterations or 1 to cap iterations at the specified maximum value." << endl;
+        cout << "Number of Queens: " << queenNum << endl;
+        cout << "Maximum number of iterations: " << maxIter << endl;
+        cout << "Iteration increment value: " << iterModifier << endl;
+    }
+    if(argc >= 2){
+      queenNum = std::stoi(argv[1]);
+    }
+    if(argc >= 3){
+      maxIter = std::stoi(argv[2]);
+    }
+    if(argc >= 4){
+      iterModifier = std::stoi(argv[3]);
+    }
+
+    Individual *best = edaLoop(queenNum, maxIter, iterModifier);
+    cout << "Best solution found: " << best->toString() << endl;
+  } catch (const std::exception& e) {
+    cout << e.what() << endl;
+  }
+
+
+  return 0;
 }
