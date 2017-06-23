692b8e93ffed415fdd2256316e04755489e20723
churmd <churmdaniel@gmail.com>
2017-06-23 16:12:02 +0100

    Improved individual comment

--- include/structures.h
+++ include/structures.h
@@ -7,6 +7,8 @@ class string;
 /**
   Purpose: An Individual represents a single solution to the N-Queens problem, with each index in the rowValues array representing a column and the values stored are the corresponding rows.
   Example: [1,3,0,2]
+  Table: Col | 0 1 2 3
+         Row | 1 3 0 2
   Coords : (0,1),(1,3),(2,0),(3,2)
   Visually:
      -> X
